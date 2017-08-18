5cebee538fc1f2f8c50016275ec1e126181f024c
naazy <marziyeh.naaz.ahmed@gmail.com>
2017-08-18 13:43:01 +0100

    hopefully fixing CI

--- mix.exs
+++ mix.exs
@@ -60,6 +60,6 @@ defmodule Dwylbot.Mixfile do
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
      "ecto.reset": ["ecto.drop", "ecto.setup"],
-     "test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+     "test": ["ecto.create ", "ecto.migrate", "run priv/repo/seeds.exs", "test"]]
   end
 end
